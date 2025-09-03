25eb65f754042e7c9454deb9bd5bc01071027bfb
syafiq-f <syafiq.faqih@ui.ac.id>
2025-09-03 16:00:38 +0700

    Complete tutorial 1: Django MVT implementation

--- main/views.py
+++ main/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'npm' : '240123456',
-        'name': 'Haru Urara',
-        'class': 'PBP A'
+        'npm' : '2406439715',
+        'name': 'Syafiq Faqih',
+        'class': 'PBP C'
     }
 
     return render(request, "main.html", context)
