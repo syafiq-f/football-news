e37603d35d62daa166ba34384986f9c08fd66578
syafiq-f <syafiq.faqih@ui.ac.id>
2025-09-03 15:54:43 +0700

    Complete tutorial 1: Django MVT implementation

--- football_news/urls.py
+++ football_news/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('http://localhost:8000/', include('main.urls')),
+    path('', include('main.urls')),
 ]
