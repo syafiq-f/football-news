b24a84d75fde0c623124a6a5e0aec5e82ab7426b
syafiq-f <syafiq.fqh@gmail.com>
2025-09-10 16:30:44 +0700

    Tutorial 2 selesai

--- main/views.py
+++ main/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from main.forms import NewsForm
 from main.models import News
-
+from django.http import HttpResponse
+from django.core import serializers
 def show_main(request):
     news_list = News.objects.all()
 
@@ -33,3 +34,31 @@ def show_news(request, id):
     }
 
     return render(request, "news_detail.html", context)
+
+def show_xml(request):
+     news_list = News.objects.all()
+     xml_data = serializers.serialize("xml", news_list)
+     return HttpResponse(xml_data, content_type="application/xml")
+
+def show_json(request):
+    news_list = News.objects.all()
+    json_data = serializers.serialize("json", news_list)
+    return HttpResponse(json_data, content_type="application/json")
+
+
+def show_xml_by_id(request, news_id):
+   try:
+       news_item = News.objects.filter(pk=news_id)
+       xml_data = serializers.serialize("xml", news_item)
+       return HttpResponse(xml_data, content_type="application/xml")
+   except News.DoesNotExist:
+       return HttpResponse(status=404)
+
+def show_json_by_id(request, news_id):
+   try:
+       news_item = News.objects.get(pk=news_id)
+       json_data = serializers.serialize("json", [news_item])
+       return HttpResponse(json_data, content_type="application/json")
+   except News.DoesNotExist:
+       return HttpResponse(status=404)
+   
