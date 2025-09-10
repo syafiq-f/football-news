e2d40fdb8f9bdd2be909dc0224b63c91d2053289
syafiq-f <syafiq.fqh@gmail.com>
2025-09-10 15:57:14 +0700

    help

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from main.views import show_main
+from main.views import show_main, create_news, show_news
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
+    path('create-news/', create_news, name='create_news'),
+    path('news/<str:id>/', show_news, name='show_news'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,10 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from main.forms import NewsForm
+from main.models import News
 
 def show_main(request):
+    news_list = News.objects.all()
+
     context = {
         'npm' : '2406439715',
         'name': 'Syafiq Faqih',
-        'class': 'PBP C'
+        'class': 'PBP C',
+        'news_list': news_list
     }
 
     return render(request, "main.html", context)
+
+def create_news(request):
+    form = NewsForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('main:show_main')
+
+    context = {'form': form}
+    return render(request, "create_news.html", context)
+
+def show_news(request, id):
+    news = get_object_or_404(News, pk=id)
+    news.increment_views()
+
+    context = {
+        'news': news
+    }
+
+    return render(request, "news_detail.html", context)
